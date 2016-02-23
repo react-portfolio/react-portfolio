2006b4caa630c6d4658fb244a4859160b083262e
JC <jc@jcmbp.local>
2016-02-22 16:32:51 -0800

    added projects reducer and projects data store

---
+++ src/data/projects.js
@@ -0,0 +1,15 @@
+const projectsData = {
+  data: [{
+    desktop_image: 'image_url',
+    mobile_image: 'image_url',
+    title: 'title',
+    date: 'year',
+    github_url: 'url',
+    project_url: 'url',
+    background: 'background',
+    role: 'role',
+    tech_stack: 'tech_stack'
+  }]
+};
+
+export { projectsData };

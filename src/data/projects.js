89ff2f699744a77ad4aeae46b5bc02741138cf6e
JC <jc@jcmbp.local>
2016-02-22 21:10:49 -0800

    added more fake projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,15 +1,50 @@
 const projectsData = {
-  data: [{
-    desktop_image: 'image_url',
-    mobile_image: 'image_url',
-    title: 'title',
-    date: 'year',
-    github_url: 'github_url',
-    project_url: 'project_url',
-    background: 'ba;waibubackground iaberli;bj;bj;bj;bj;a;waibubackground iaberli;bj;bj;ba;waibubackground iaberli;bj;bj;bj;bj;bj;bj;bbjba;waibubackground iaberli;bj;bj;bj;bj;a;waibubackground iaberli;bj;bj;ba;waibubackground iaberli;bj;bj;bj;bj;bj;bj;bbj',
-    role: 'role background iaberliubliabuole background iaberliubliole background iaberliubliabu;iababu;iab;iabu',
-    tech_stack: ['React', 'socket.io', 'Redux', 'PostgreSQL']
-  }]
+  data: [
+    {
+      desktop_image: 'image_url',
+      mobile_image: 'image_url',
+      title: 'title',
+      date: 'year',
+      github_url: 'url',
+      project_url: 'url',
+      background: 'ba;waibubackground iaberli;bj;bj;bj;bj;a;waibubackground iaberli;bj;bj;ba;waibubackground iaberli;bj;bj;bj;bj;bj;bj;bbjba;waibubackground iaberli;bj;bj;bj;bj;a;waibubackground iaberli;bj;bj;ba;waibubackground iaberli;bj;bj;bj;bj;bj;bj;bbj',
+      role: 'role background iaberliubliabuole background iaberliubliole background iaberliubliabu;iababu;iab;iabu',
+      tech_stack: ['React', 'socket.io', 'Redux', 'PostgreSQL']
+    },
+    {
+      desktop_image: 'image_url',
+      mobile_image: 'image_url',
+      title: 'title',
+      date: 'year',
+      github_url: 'url',
+      project_url: 'url',
+      background: 'ba;waibubackground iaberli;bj;bj;bj;bj;a;waibubackground iaberli;bj;bj;ba;waibubackground iaberli;bj;bj;bj;bj;bj;bj;bbj',
+      role: 'role background iaberliubliabuole background iaberliubliole background iaberliubliabu;iababu;iab;iabu',
+      tech_stack: ['React', 'socket.io', 'Redux', 'PostgreSQL']
+    },
+    {
+      desktop_image: 'image_url',
+      mobile_image: 'image_url',
+      title: 'title',
+      date: 'year',
+      github_url: 'url',
+      project_url: 'url',
+      background: 'ba;waibubackground iaberli;bj;bj;bj;bj;a;waibubackground iaberli;bj;bj;ba;waibubackground iaberli;bj;bj;bj;bj;bj;bj;bbj',
+      role: 'role background iaberliubliabuole background iaberliubliole background iaberliubliabu;iababu;iab;iabu',
+      tech_stack: ['React', 'socket.io', 'Redux', 'PostgreSQL']
+    },
+    {
+      desktop_image: 'image_url',
+      mobile_image: 'image_url',
+      title: 'title',
+      date: 'year',
+      github_url: 'url',
+      project_url: 'url',
+      background: 'ba;waibubackground iaberli;bj;bj;bj;bj;a;waibubackground iaberli;bj;bj;ba;waibubackground iaberli;bj;bj;bj;bj;bj;bj;bbj',
+      role: 'role background iaberliubliabuole background iaberliubliole background iaberliubliabu;iababu;iab;iabu',
+      tech_stack: ['React', 'socket.io', 'Redux', 'PostgreSQL']
+    },
+  ]
 };
 
 export { projectsData };

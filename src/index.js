637f37f998da3d6eaa276748f2032e6408f0a381
Rachel RoseFigura <rachel.rosefigura@gmail.com>
2016-03-03 11:07:21 -0800

    remove personal info

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-/**
+/*
  * Import the stylesheet you want used! Here we just reference
  * the main SCSS file we have in the styles directory.
  */
 import './styles/main.scss';
 
-/**
+/*
  * Both configureStore and Root are required conditionally.
  * See configureStore.js and Root.js for more details.
  */
+
 import { configureStore } from './store/configureStore';
 import { Root } from './containers/Root';
 
